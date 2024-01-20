8b5a50bfa324830673f1df132891b06b71f1c10d
MacdonaldRobinson <maocnly2001@gmail.com>
2024-01-20 12:25:14 -0700

    - Added Scene loader and improved the world selector

---
+++ ui/SceneLoader/SceneLoader.gd
@@ -0,0 +1,42 @@
+extends Control
+class_name SceneLoader
+
+@onready var scene_container: Node3D = $SceneContainer
+@onready var progress_ui: Label = $Progress
+@onready var start: Button = $Start
+
+var scene_to_load: String
+var scene_load_status = 0
+var progress: Array = []
+var callback: Callable
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	start.hide()
+	pass
+	
+func load_scene(scene_to_load: String, callback: Callable):
+	self.scene_to_load = scene_to_load
+	self.callback = callback
+	
+	ResourceLoader.load_threaded_request(self.scene_to_load)
+	
+func _process(delta):
+	if not scene_to_load:
+		return
+		
+	scene_load_status = ResourceLoader.load_threaded_get_status(scene_to_load, progress)
+	progress_ui.text = str(progress[0] * 100) + "%"
+
+	if scene_load_status == ResourceLoader.THREAD_LOAD_LOADED:
+		start.show()
+
+func _on_start_pressed():
+	var loaded_scene = ResourceLoader.load_threaded_get(scene_to_load)	
+	var scene_instance: Node = loaded_scene.instantiate()
+	scene_container.add_child(scene_instance)
+	callback.call(scene_instance)
+	
+	scene_instance.reparent(get_tree().root)	
+	get_tree().root.remove_child(self)
+
